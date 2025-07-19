d817a69c87f0c6e6626fd9ba292f3c1d5a628f4f
chris194aniel <chris194aniel@gmail.com>
2025-07-18 22:58:47 -0600

    Boton

--- Veterinaria/obj/Debug/net8.0-windows/Veterinaria.AssemblyInfo.cs
+++ Veterinaria/obj/Debug/net8.0-windows/Veterinaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f41d654d73e1a38d131963fe614ebf17b0d78c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+501f0aacad71d7461b5530bb03ee5a1912d208e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Veterinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
